1626b7f758757e30c36a5e76873b688e13ad076b
simpart <simpart>
2018-11-15 02:22:08 +0000

    add comment

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 /**
  * @file mofron-effect-fade/index.js
+ * @brief fade effect for mofron
  * @author simpart
  */
-
-/**
- * @class Fade
- * @brief fade effect class
- */
-mofron.effect.Fade = class extends mofron.Effect {
-    
+const mf = require('mofron');
+mf.effect.Fade = class extends mf.Effect {
+    /**
+     * initialize fade effect
+     *
+     * @param p1 (number) effect speed (second)
+     * @param p1 (object) effect option
+     */
     constructor (po) {
         try {
             super(po);
             this.name('Fade');
             this.prmMap('speed');
+            this.speed(0.7);
             this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
@@ -21,11 +24,16 @@ mofron.effect.Fade = class extends mofron.Effect {
         }
     }
     
+    /**
+     * setting opacity css
+     *
+     * @note private method
+     */
     execute (flg) {
         try {
             if ( (true === this.status()) &&
-                 ('0' !== this.component().style('opacity')) ) {
-                this.component().style({ 'opacity' : '0' });
+                 ('0' !== this.component().adom().style('opacity')) ) {
+                this.component().adom().style({ 'opacity' : '0' });
             }
             super.execute(flg);
         } catch (e) {
@@ -35,41 +43,28 @@ mofron.effect.Fade = class extends mofron.Effect {
     }
     
     /**
-     * fade in
+     * fade in target component
+     *
+     * @note private method
      */
     enable (tgt) {
-        try {
-console.log(tgt);
-            tgt.adom().style({'opacity' : '1'});
-        } catch (e) {
+        try { tgt.adom().style({'opacity' : '1'}); } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
     /**
-     * fade out
+     * fade out target component
+     *
+     * @note private method
      */
     disable (tgt) {
-        try {
-            tgt.adom().style({'opacity' : '0'});
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
-    
-    speed (prm) {
-        try {
-            if (undefined === prm) {
-                return (0 === super.speed()) ? 0.7 : super.speed();
-            }
-            super.speed(prm);
-        } catch (e) {
+        try { tgt.adom().style({'opacity' : '0'}); } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
 }
-module.exports = mofron.effect.Fade;
+module.exports = mf.effect.Fade;
 /* end of file */
